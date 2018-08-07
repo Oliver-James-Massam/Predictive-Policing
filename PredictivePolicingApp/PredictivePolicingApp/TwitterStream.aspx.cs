84a9348ab911355e8fb9b90f0b56c67180e0d0fa
ojmassam <ojmassam@gmail.com>
2018-08-07 09:06:42 +0200

    Changed Twitter Keys and removed them visably from the project

--- PredictivePolicingApp/PredictivePolicingApp/TwitterStream.aspx.cs
+++ PredictivePolicingApp/PredictivePolicingApp/TwitterStream.aspx.cs
@@ -6,20 +6,21 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using TweetSharp;
 using System.Timers;
+using PredictivePolicingApp.Code.Misc;
 
 
 namespace PredictivePolicingApp
 {
     public partial class TwitterStream : System.Web.UI.Page
     {
-        private static String customer_key = "yunXqY7I12KUGMpaZs7E5p5jL";
-        private static String customer_key_secret = "Tv5Hwso9hGEeDRhExYpnUOQ1tVuDuabOUupA16YtsY6VAeMvde";
-        private static String access_token = "1499220372-9pAdkw7AeXNjoi5XMPZTI2hoZ8ZQyhJBAb09QNU";
-        private static String access_token_secret = "TfhOO2dWTkHM31aj1qDINq9EFArDLwERoZlVVhzm8dDCp";
+        private static String customer_key = TwitterKeys.getCustomerKey();
+        private static String customer_key_secret = TwitterKeys.getCustomerKeySecret();
+        private static String access_token = TwitterKeys.getAccessToken();
+        private static String access_token_secret = TwitterKeys.getAccessTokenSecret();
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
